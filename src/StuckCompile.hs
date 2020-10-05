70e2b2ffd43e5fe84ad4c82bf760a6fc2db270ca
Conrad Khakhria <conradjkhakhria@gmail.com>
2020-10-05 16:45:00 +0100

    added gitignore

--- src/StuckCompile.hs
+++ src/StuckCompile.hs
@@ -13,13 +13,13 @@ data CompileState =
 
 compileMaths :: ParseTree -> Integer -> [String] -> String
 compileMaths (NUM n) args lineNumber
-  | "." `elem` n = error $ pLine ++ ": floating point numbers not supported"
-  | is32bits     = error $ pLine ++ ": numbers must be 32 bits"
-  | otherwise    = "push" ++ n
-  where pLine    = "Line " ++ show lineNumber
-        is32bits = abs (read n :: Integer) > 2147483647
+  | "." `elem` n  = error $ pLine ++ ": floating point numbers not supported"
+  | not32bits     = error $ pLine ++ ": numbers must be 32 bits"
+  | otherwise     = "push" ++ n
+  where pLine     = "Line " ++ show lineNumber
+        not32bits = abs (read n :: Integer) > 2147483647
 
 compileMaths (SYM a) args lineNumber
   | 
-  where argIndex = elemIndex a args
+  where argIndex = elemIndexs a args
 
