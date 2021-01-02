0884e90eb9410aaac1ae0eb0b7f92cedbaee060c
Conrad Khakhria <conradjkhakhria@gmail.com>
2021-01-02 16:36:15 +0000

    Now compiles to C instead of assembly

--- src/StuckCompile.hs
+++ src/StuckCompile.hs
@@ -1,7 +1,7 @@
 module StuckCompile where
 import qualified Data.Map as Map
 import Data.Maybe (fromJust)
-import Data.List  (elemIndex)
+import Data.List  (elemIndex, intercalate)
 
 import Parsing
 import StuckParse
@@ -19,30 +19,46 @@ argOffset n = show $ 4 * (n + 1)
 
 {- Compiles ParseTrees -}
 
+--compileMaths :: ParseTree -> [String] -> Int -> String
+--compileMaths (NUM n) args lineNum
+--  | '.' `elem` n  = error $ pLine ++ ": floating point numbers not supported"
+--  | not64bits     = error $ pLine ++ ": numbers must be 32 bits"
+--  | otherwise     = "push "  ++ n ++ "\n"
+--  where pLine     = "Line " ++ show lineNum ++ "\n"
+--        not32bits = abs (read n :: Integer) > 0x8000000000000000
+--
+--compileMaths (SYM a) args lineNum = "push dword [ebp + " ++ show argOffset ++ "]\n"
+--  where argIndex  = fromJust $ elemIndex a args
+--        argOffset = 4 * argIndex + 4
+--
+--compileMaths (NODE {pOperation = op, pElems = elems}) args lineNum =
+--  pushElems ++ "mov eax, [esp]\n" ++ combine ++ cleanup
+--  where pushElems = concat $ reverse [compileMaths e args lineNum | e <- elems]
+--        movEBX    = [ "mov ebx, [esp+" ++ show (i*4) ++ "]\n" | i <- [1..(length args)] ]
+--        combine   = concat [mov ++ opInst | mov <- movEBX]
+--        cleanup   = "add esp, " ++ show (4 * (length args - 1)) ++ "\nmov [esp], eax\n"
+--        opInst    = case op of
+--                      "+" -> "add eax, ebx\n"
+--                      "-" -> "sub eax, ebx\n"
+--                      "*" -> "mul ebx\n"
+--                      "/" -> "xor edx, edx\ndiv ebx"
+--                      "^" -> error "Exponents not yet implemented"
+
 compileMaths :: ParseTree -> [String] -> Int -> String
 compileMaths (NUM n) args lineNum
-  | '.' `elem` n  = error $ pLine ++ ": floating point numbers not supported"
-  | not32bits     = error $ pLine ++ ": numbers must be 32 bits"
-  | otherwise     = "push "  ++ n ++ "\n"
+  | '.' `elem` n = error $ pLine ++ ": floating point numbers not supported"
+  | not64bits    = error $ pLine ++ ": numbers must be 64 bits"
+  | otherwise    = n
   where pLine     = "Line " ++ show lineNum ++ "\n"
-        not32bits = abs (read n :: Integer) > 2147483647
+        not64bits = abs (read n :: Integer) > 0x8000000000000000
 
-compileMaths (SYM a) args lineNum = "push dword [ebp + " ++ show argOffset ++ "]\n"
-  where argIndex  = fromJust $ elemIndex a args
-        argOffset = 4 * argIndex + 4
+compileMaths (SYM s) args lineNum = s
 
-compileMaths (NODE {pOperation = op, pElems = elems}) args lineNum =
-  pushElems ++ "mov eax, [esp]\n" ++ combine ++ cleanup
-  where pushElems = concat $ reverse [compileMaths e args lineNum | e <- elems]
-        movEBX    = [ "mov ebx, [esp+" ++ show (i*4) ++ "]\n" | i <- [1..(length args)] ]
-        combine   = concat [mov ++ opInst | mov <- movEBX]
-        cleanup   = "add esp, " ++ show (4 * (length args - 1)) ++ "\nmov [esp], eax\n"
-        opInst    = case op of
-                      "+" -> "add eax, ebx\n"
-                      "-" -> "sub eax, ebx\n"
-                      "*" -> "mul ebx\n"
-                      "/" -> "xor edx, edx\ndiv ebx"
-                      "^" -> error "Exponents not yet implemented"
+compileMaths NODE { pOperation = op, pElems = elems } args lineNum =
+  if op == "^" then
+    error "Exponents not yet implemented"
+  else
+    intercalate op [compileMaths e args lineNum | e <- elems]
 
 {- Compile Instructions -}
 
@@ -51,40 +67,35 @@ compileEnd 0 = "" -- Doesn't print label zero (which only occured at the end of 
 compileEnd n = ".JMP_" ++ show n ++ ":\n"
 
 compilePush :: Instruction -> String
-compilePush (PUSH i _) = pushL1 ++ pushL2 ++ pushL3
-  where pushL1 = "mov edx, [ebp+" ++ argOffset i ++ "]\n"
-        pushL2 = "mov [STACK+esi], edx\n"
-        pushL3 = "add esi, 4\n"
+compilePush (PUSH a _) = "STACK[STACK_INDEX++] = " ++ a ++ ";\n"
 
 compilePop :: Instruction -> String
-compilePop (POP i _) = popL1 ++ popL2 ++ popL3
-  where popL1  = "mov edx, [STACK+esi-4]\n"
-        popL2  = "mov dword [ebp+" ++ argOffset i ++ "], edx\n"
-        popL3  = "sub esi, 4\n"
+compilePop (POP a _) = a ++ " = STACK[--STACK_INDEX];\n"
 
 compileCond :: Instruction -> CompileState -> String
-compileCond (COND i _) state = condL1 ++ condL2
-  where condL1 = "cmp dword [ebp+" ++ argOffset i ++ "], 0\n"
-        condL2 = "jng .JMP_" ++ show n ++ "\n"
-        n      = sJmpCount state
+compileCond (COND a _) state = "if (" ++ a ++ " > 0) {\n"
+
+-- compileCall :: Instruction -> [String] -> String
+-- compileCall (CALL name as lineNo) args =
+--   callL1 ++ callL2 ++ callL3
+--   where callL1 = concat [ compileMaths a args lineNo | a <- as ]
+--         callL2 = "call func_" ++ name ++ "\n"
+--         callL3 = "add esi, "  ++ show (4 * length args) ++ "\n"
 
 compileCall :: Instruction -> [String] -> String
 compileCall (CALL name as lineNo) args =
-  callL1 ++ callL2 ++ callL3
-  where callL1 = concat [ compileMaths a args lineNo | a <- reverse as ]
-        callL2 = "call func_" ++ name ++ "\n"
-        callL3 = "add esi, "  ++ show (4 * length args) ++ "\n"
+  "func_" ++ name ++ "(" ++ intercalate ", " [ compileMaths a args lineNo | a <- as ] ++ ");\n"
 
 compileInstructions :: [Instruction] -> FMAP -> CompileState -> String
 compileInstructions [] _ _ = ""
 compileInstructions (i : is) m state
-  | isEnd    i = compileEnd n               ++ next eState
-  | isInput  i = "call user_input\n"        ++ next state
-  | isOutput i = "call user_output\n"       ++ next state
-  | isPush   i = compilePush i              ++ next state
-  | isPop    i = compilePop  i              ++ next state
-  | isCond   i = compileCond i state        ++ next cState
-  | isCall   i = compileCall i clArgs       ++ next state
+  | isEnd    i = "}\n"                ++ next eState
+  | isInput  i = "uinput();\n"        ++ next state
+  | isOutput i = "uoutput();\n"       ++ next state
+  | isPush   i = compilePush i        ++ next state
+  | isPop    i = compilePop  i        ++ next state
+  | isCond   i = compileCond i state  ++ next cState
+  | isCall   i = compileCall i clArgs ++ next state
   where n      = if null is then 0 else head $ sJmpStack state
         clArgs = fromJust $ Map.lookup (cFuncName i) m
         next   = compileInstructions is m
@@ -95,9 +106,9 @@ compileInstructions (i : is) m state
 compileFunctions :: [Function] -> FMAP -> String
 compileFunctions [] _       = ""
 compileFunctions (f : fs) m =
-  s ++ compileInstructions (fBody f) m state ++ e ++ compileFunctions fs m
-  where s     = "func_" ++ fName f ++ ":\npush ebp\nmov ebp, esp\n"
-        e     = "mov esp, ebp\npop ebp\nret\n"
+  n ++ as ++ ") {\n" ++ compileInstructions (fBody f) m state ++ compileFunctions fs m
+  where n     = "void func_" ++ fName f ++ "("
+        as    = "int64_t " ++ intercalate ", int64_t " (fArgs f)
         state = (CompileState { sFuncName = fName f
                               , sFuncArgs = fArgs f
                               , sJmpStack = []
@@ -105,55 +116,52 @@ compileFunctions (f : fs) m =
 
 boilerplate1 :: String
 boilerplate1 = "\
-\global main                  \n\
-\extern printf                \n\
-\extern read                  \n\
-\extern atoi                  \n\
-\segment .data                \n\
-\fmt: db \"%d\", 10, 0        \n\
-\segment .bss                 \n\
-\STACK: resd 0x10000          \n\
-\INBUF: resd 0x100            \n\
-\segment .text                \n\
-\user_output:                 \n\
-\push  ebp                    \n\
-\mov   ebp, esp               \n\
-\push  dword [STACK+esi-4]    \n\
-\push  fmt                    \n\
-\call  printf                 \n\
-\add   esp, 8                 \n\
-\mov   esp, ebp               \n\
-\pop   ebp                    \n\
-\ret                          \n\
-\user_input:                  \n\
-\push  ebp                    \n\
-\mov   ebp, esp               \n\
-\push  0x100                  \n\
-\push  INBUF                  \n\
-\push  0                      \n\
-\call  read                   \n\
-\add   esi, 12                \n\
-\push  INBUF                  \n\
-\call  atoi                   \n\
-\mov   [STACK+esi], eax       \n\
-\add   esi, 4                 \n\
-\mov   esp, ebp               \n\
-\pop   ebp                    \n\
-\ret                          \n"
+\#include <stdio.h>                             \n\
+\#include <stdint.h>                            \n\
+\                                               \n\
+\int64_t  STACK[0x100000];                      \n\
+\uint64_t STACK_INDEX;                          \n\
+\                                               \n\
+\int64_t ipow(int64_t n, int64_t r)             \n\
+\{                                              \n\
+\    int64_t retval = 1;                        \n\
+\                                               \n\
+\    for (int i = 0; i < sizeof(r); i++) {      \n\
+\         if (r & 1 << i) {                     \n\
+\            int64_t comp = n;                  \n\
+\                                               \n\
+\            for (int c = 0; c < i; c++) {      \n\
+\                comp *= comp;                  \n\
+\            }                                  \n\
+\                                               \n\
+\            retval *= comp;                    \n\
+\        }                                      \n\
+\    }                                          \n\
+\                                               \n\
+\    return retval;                             \n\
+\}                                              \n\
+\                                               \n\
+\void uinput()                                  \n\
+\{                                              \n\
+\    int64_t input_val;                         \n\
+\                                               \n\
+\    while (scanf(\"%ld\", &input_val) == EOF) {\n\
+\        fprintf(stderr, \"Invalid input\\n\"); \n\
+\    }                                          \n\
+\                                               \n\
+\    STACK[STACK_INDEX++] = input_val;          \n\
+\}                                              \n\
+\                                               \n\
+\void uoutput()                                 \n\
+\{                                              \n\
+\    printf(\"%ld\\n\", STACK[STACK_INDEX - 1]);\n\
+\}\n"
 
-boilerplate2 :: String -> String -> String
-boilerplate2 lFuncName argcOffset = "\
-\main:                          \n\
-\push  ebp                      \n\
-\mov   ebp, esp                 \n\
-\xor   eax, eax                 \n\
-\xor   ebx, ebx                 \n\
-\xor   esi, esi                 \n\
-\mov   dword [STACK+esi], 5     \n\
-\add   esi, 4                   \n\
-\sub   esp, " ++ argcOffset ++ "\n\
-\call  func_" ++ lFuncName  ++ "\n\
-\add   esp, " ++ argcOffset ++ "\n\
-\mov   eax, 1                   \n\
-\mov   ebx, 0                   \n\
-\int   0x80                     \n"
+
+boilerplate2 :: String -> Int -> String
+boilerplate2 lastFuncName argCount =
+  let
+    funcall = lastFuncName ++ "(" ++ init (concat (replicate argCount "0,")) ++ ");\n"
+  in
+    "int main() {\n STACK_INDEX = 0; \nfunc_" ++ funcall ++ "}"
+
